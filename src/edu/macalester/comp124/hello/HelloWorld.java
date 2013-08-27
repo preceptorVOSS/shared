f09de27e84880445524c559d5b019ad316725014
shilad <shilad@gmail.com>
2013-08-27 16:08:19 -0500

    Initial import.

---
+++ src/edu/macalester/comp124/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.macalester.comp124.hello;
+
+/**
+ * @author Shilad Sen
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello, world!");
+    }
+}
