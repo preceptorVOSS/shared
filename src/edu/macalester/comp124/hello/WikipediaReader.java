eeb047d0cf464bbb7f9573caaada1b3979ef18a6
shilad <shilad@gmail.com>
2013-09-06 11:07:26 -0500

    Added Wikipedia Reader

---
+++ src/edu/macalester/comp124/hello/WikipediaReader.java
@@ -0,0 +1,21 @@
+package edu.macalester.comp124.hello;
+
+import acm.program.DialogProgram;
+import net.sourceforge.jwbf.core.contentRep.Article;
+import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+
+/**
+ * @author Shilad Sen
+ */
+public class WikipediaReader extends DialogProgram {
+    public void run() {
+        MediaWikiBot bot = new MediaWikiBot("http://en.wikipedia.org/w/");
+        String page = this.readLine("Enter page that should be fetched");
+        Article article = bot.getArticle(page);
+        String contents = article.getText();
+        if (contents.length() > 1000) {
+            contents = contents.substring(0, 1000);
+        }
+        println(contents);
+    }
+}
