ffccb8f52b504548d885be06dfb251a5d06e7753
shilad <shilad@gmail.com>
2013-09-17 01:25:29 -0500

    Added PalindromeTest class.

---
+++ test/edu/macalester/comp124/palindrome/PalindromeTest.java
@@ -0,0 +1,23 @@
+package edu.macalester.comp124.palindrome;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class PalindromeTest {
+
+    @Test
+	public void testPalindrome1() {
+        // After you finish, you should be able to uncomment these lines and pass the test.
+//		Palindrome pal = new Palindrome();
+//		assertTrue(pal.isPalindrome("racecar"));
+//		assertFalse(pal.isPalindrome("racecarz"));
+	}
+	
+	/*
+	 * Write your own test methods below to test for fancier palindromes.
+	 * Like the method above, each method MUST start with the word "test."
+	 */
+}
