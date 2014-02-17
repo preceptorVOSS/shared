4a71df6b213c5bcffecf3592733aa4bb76ebc828
shilad <shilad@gmail.com>
2014-02-17 14:22:44 -0600

    Added sky drawings.

--- src/edu/macalester/comp124/objects1/Shape.java
+++ src/edu/macalester/comp124/objects1/Shape.java
@@ -11,10 +11,21 @@ import java.awt.*;
 public class Shape extends GCompound {
 //    public Shape() {}
 
+    public void drawHole() {
+        this.drawHole(Color.BLACK);
+    }
+
     public void drawHole(Color color) {
         double height = getHeight();
         double width = getWidth();
         double radius = Math.min(height, width) / 3;
+        this.drawHole(color, radius);
+    }
+
+
+    public void drawHole(Color color, double radius) {
+        double height = getHeight();
+        double width = getWidth();
 
         GOval hole = new GOval(radius*2, radius*2);
         hole.setFillColor(color);
