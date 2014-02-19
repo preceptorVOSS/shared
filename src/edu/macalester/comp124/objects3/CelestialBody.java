f2d0138680a7407a64bc64da81e662cd1548e236
shilad <shilad@gmail.com>
2014-02-19 11:04:11 -0600

    Added sky drawings.

--- src/edu/macalester/comp124/objects3/CelestialBody.java
+++ src/edu/macalester/comp124/objects3/CelestialBody.java
@@ -11,6 +11,10 @@ import java.awt.*;
 public class CelestialBody extends GCompound {
 //    public CelestialBody() {}
 
+    public void makeHole() {
+        makeHole(Color.BLACK);
+    }
+
     public void makeHole(Color color) {
         double radius = getWidth() / 4;
         GOval oval = new GOval(radius*2, radius*2);
