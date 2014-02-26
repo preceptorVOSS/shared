51c006dd0e916e8773d714f4ac92b33845d8558c
baylor <baylorw>
2014-02-26 11:05:57 -0600

    Git comments are dumb

---
+++ src/edu/macalester/comp124/baylor/inheritance/Application.java
@@ -0,0 +1,15 @@
+package edu.macalester.comp124.baylor.inheritance;
+
+import acm.program.ConsoleProgram;
+
+/**
+ * Created by baylor on 2/26/14.
+ */
+public class Application
+	extends ConsoleProgram
+{
+	public void run()
+	{
+
+	}
+}
